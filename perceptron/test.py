1c013a6f1633a6b1718267c8f7de92791f341d15
pradeepgangwar <pradeepgangwar39@gmail.com>
2018-08-16 13:52:26 +0530

    First perceptron

---
+++ perceptron/test.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def activation_fn(sum):
+    ''' Step Function '''
+    if (sum > 0):
+        return 1
+    return 0
+
+
+def predict(data, weights):
+    ''' Calculate the weighted sum '''
+    weighted_sum = weights.T.dot(data)
+    a = activation_fn(weighted_sum)
+    return a
+
+def test(data, weights):
+    ''' Loop through testing set and print the output'''
+    for i in range(data.shape[0]):
+        output = predict(data[i], weights)
+        print(output)
+
+if __name__=='__main__':
+
+    testing_data = np.loadtxt('test_input.txt')
+    weights = np.loadtxt('corrected_weights.txt')
+
+    test(testing_data, weights)
